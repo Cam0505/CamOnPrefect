898c864b0038168508992def26d10b2ff40f766d
Cam <cam.martin0505@gmail.com>
2025-06-01 09:21:07 +0000

    Fixing timestamp issue for snapshot

--- dbt/macros/convert_to_timezone.sql
+++ dbt/macros/convert_to_timezone.sql
@@ -1,3 +1,3 @@
 {% macro convert_to_timezone(column_name, timezone='Australia/Melbourne') %}
-    {{ column_name }} AT TIME ZONE 'UTC' AT TIME ZONE '{{ timezone }}'
+    ({{ column_name }} AT TIME ZONE 'UTC' AT TIME ZONE '{{ timezone }}')::timestamp
 {% endmacro %}
