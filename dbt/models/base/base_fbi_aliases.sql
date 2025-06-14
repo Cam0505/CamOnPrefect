e369a9576c634ec542ed80dbb444479d869733b5
cam0505 <cam.martin0505@gmail.com>
2025-06-14 03:18:34 +0000

    Sql fluff code linting, trying to only run it on new or changed sql

--- dbt/models/base/base_fbi_aliases.sql
+++ dbt/models/base/base_fbi_aliases.sql
@@ -1,3 +1,11 @@
+-- Description: Base Table for fbi most wanted - Bev Type, Alcoholic Type and Beverage Name
+-- ------------------------------------------------------------------------------
+-- Change Log:
+-- Date       | Author   | Description
+-- -----------|----------|-------------------------------------------------------
+-- 2025-05-23 | Cam      | Initial creation
+-- YYYY-MM-DD | NAME     | [Add future changes here] (Second Test, Please work)
+-- ------------------------------------------------------------------------------
 SELECT
     value AS aliases
     , fw.uid AS wanted_id
